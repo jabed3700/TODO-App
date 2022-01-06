e6d7730401c24dba6a3b50861022709f2cc32a31
tamim008bd <tamim008bd@gmail.com>
2022-01-06 11:17:04 +0600

    Project satup,boostrap

---
+++ src/Components/todos/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Todos extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 className="display-4 text-center mb-4">Todo Application</h1>
+            </div>
+        )
+    }
+}
+
+export default Todos;
