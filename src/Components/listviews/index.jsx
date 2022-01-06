5f987e7dbede122a31cb0fb088ebfa48ea9c48bb
tamim008bd <tamim008bd@gmail.com>
2022-01-06 20:26:38 +0600

    add task form

--- src/Components/listviews/index.jsx
+++ src/Components/listviews/index.jsx
@@ -23,7 +23,7 @@ const ListItem = ({todo,toggleSelect,toggleComplete})=>{
     )
 }
 
-ListItem.prototype ={
+ListItem.propTypes ={
     todo:PropTypes.object.isRequired,
     toggleSelect: PropTypes.func.isRequired,
     toggleComplete: PropTypes.func.isRequired,
@@ -45,7 +45,7 @@ const ListView = ({todos,toggleSelect,toggleComplete})=>{
     )
 }
 
-ListView.prototype ={
+ListView.propTypes ={
     todos:PropTypes.object.isRequired,
     toggleSelect: PropTypes.func.isRequired,
     toggleComplete: PropTypes.func.isRequired,
